8e8a2ac81a62d50fb7b60727645695090f096001
Sunidhi2517 <118195907+Sunidhi2517@users.noreply.github.com>
2025-04-01 23:08:06 +0530

    Update gallery.js

--- client/src/components/gallery.js
+++ client/src/components/gallery.js
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 import './gallery.css';
+import cabin1 from './assets/cabin1.jpeg';
+import cabin2 from './assets/cabin2.jpeg';
+import cabin3 from './assets/cabin3.jpeg';
+import lacker1 from './assets/lacker1.jpeg';
+import water from './assets/water.JPG';
+import dining from './assets/dining.jpeg';
+import one from './assets/one.jpeg';
+import two from './assets/two.jpeg';
+import three from './assets/three.jpeg';
+import four from './assets/four.jpeg';
+import five from './assets/five.jpeg';
+import six from './assets/six.jpeg';
+import seven from './assets/seven.jpeg';
+import eight from './assets/eight.jpeg';
+
+
+
+
+
+
+
 
 const Gallery = () => {
   const [modalIsOpen, setModalIsOpen] = useState(false);
   const [currentImage, setCurrentImage] = useState(null);
 
   const images = [
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525784/cabin1_zqvkmt.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525784/cabin2_xwlxyl.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525784/cabin3_itij3p.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525784/lacker1_blsf9z.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525788/water_ko4syb.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525785/dining_drumrm.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525785/one_itgleq.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525786/two_sq71qu.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525786/three_gdzljp.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525783/four_iqhmkn.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525785/five_nmqep9.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525786/six_x9hml3.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525785/seven_eogg0z.jpg",
-    "https://res.cloudinary.com/dt3rlyga5/image/upload/v1743525785/eight_jreopq.jpg"
+     cabin1, cabin2, cabin3,lacker1,water,dining,one,two,three,four,five,six,seven,eight
+
+    
+   
+   
   ];
 
   const openModal = (image) => {
@@ -34,14 +46,15 @@ const Gallery = () => {
 
   return (
     <div className="gallery-container features">
-      <h3>Picture Gallery</h3>
-      <span>Have a look</span>
+
+        <h3>Picture Gallery</h3>
+        <span>Have a look</span>
       <div className="gallery">
         {images.map((image, index) => (
           <div className="gallery-item" key={index}>
             <img
               src={image}
-              alt={`Gallery item ${index + 1}`}
+              alt={Gallery item ${index + 1}}
               onClick={() => openModal(image)}
             />
           </div>
